ebc0997923da8bed52e5b8223c07d05633d98d1c
abdellatif tijani <tijani.idrissi.abdellatif@gmail.com>
2025-05-15 13:37:01 +0100

    final

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,22 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript',"prettier"],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-unused-expressions":"off",
+      "@typescript-eslint/no-explicit-any":"off",
+      "@typescript-eslint/no-non-null-assertion":"off",
+      "@typescript-eslint/no-empty-function":"off",
+      "@typescript-eslint/no-var-requires":"off",
+      "@typescript-eslint/no-inferrable-types":"off"
+      
+      
+    },
+  }),
 ];
 
 export default eslintConfig;
